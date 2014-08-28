0cfab26e3f8fe470603df3c8b1b668a5298ad12f
Nathan <Nathan@Nathans-MacBook-Pro.local>
2014-08-28 14:06:58 -0700

    Kali Flash Utility
    A utility to flash Kali Linux to Nexus 7 2013 (Flo/Dab), Nexus 7 2012
    (Grouper/Tilapia), and Nexus 5 (Hammerhead).

---
+++ Start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+printf '\033[8;27;100t'
+clear
+read -p "Is this a Mac or Linux computer? (Mac/Linux) "
+case "$choice" in
+
+mac|Mac|MAC )
+clear
+echo "No dependencies required. Contiuing..."
+echo "";;
+
+linux|Linux|LINUX )
+clear
+echo "Installing cURL via apt-get. Please enter your password when prompted!"
+sudo apt-get -qq update && sudo apt-get -qq install curl
+echo "";;
+esac
+
+echo "Downloading Lates KFU script..."
+echo ""
+curl -o ~/Desktop/kfu.sh 'https://0353c053bc2aaba73d57c10250836828c9338e1a.googledrive.com/host/0B4kCH1jSvs-QeUVlN3hVOFBGNTA'
+clear
+chmod 755 ~/Desktop/Kali/kfu.sh
+sh ~/Desktop/kfu.sh
