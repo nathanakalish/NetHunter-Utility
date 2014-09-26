6c721b155ea146c5aae34a7c67b1884f86911662
Photonicgeek <nathankalish@me.com>
2014-09-26 16:36:03 -0700

    ver+

--- kfu.sh
+++ kfu.sh
@@ -10,10 +10,12 @@ escript=`sed -n 2p $self`
 
 if [[ "$dscript" == "$escript" ]];
 then
+	clear
 	echo ""
 	echo "Everything is up to date!"
 	sleep 3
 else
+	clear
 	echo "Updating..."
 	rm -rf $self
 	mv /tmp/kfu.sh $self
